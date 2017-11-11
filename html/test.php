e7337c25697e18ca00b0684d61e43813842527e0
Michael <michael.morgan-4@student.manchester.ac.uk>
2017-11-11 22:01:25 +0000

    WORKING PHPgit add html/*!

---
+++ html/test.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Stress Thing</title>
+<?php
+  echo "Hello world\n";
+?>
+
+<?php
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "hack17";
+$password = "!t0SA0W'Af613(~";
+$dbname = "hackdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error)
+{
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+$query = "SELECT * FROM users";
+mysqli_query($conn, $query) or die("Q Error");
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result);
+echo $row["Username"]
+?>
+
+</body>
+</html>
+<!--
+
+$sql = "SELECT Username, VRChoice, VoiceChoice FROM users WHERE PersonNumber='1';";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["Username"]. "<br>VR Choice" . $row["VRChoice"]. "Voice Choice" . $row["VoiceChoice"] "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+-->
