5e092925a5ed4d6ba2dd99c38870e02e08744a1a
Michael <michael.morgan-4@student.manchester.ac.uk>
2017-11-12 10:27:18 +0000

    First working backend

--- audio.php
+++ audio.php
@@ -15,31 +15,63 @@ input.button
 }
 </style>
 
+<script>
+var beautifulBeachText = "Welcome to the beautiful beach. Relax. Take in the scenery.";
+var fantasticalForestText = "Welcome to the fantastical forest! Relax. Take in the scenery";
+</script>
+
 <?php
 $name = $_GET['UserName'];
-echo "<script type='text/javascript'> var name = " . $name . ";</script>";
-?>
 
-<!--
-<div>
-  <object beautifulBeachText="resources/beautiful_beach.txt"></object>
-  <object fantasticalForestText="resources/fantastical_forest.txt"></object>
-</div>
--->
+$servername = "localhost";
+$username = "hack17";
+$password = "RY3A%NFyZtYag*PV";
+$dbname = "hackdb";
+
+// Create connection to mysql database.
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Checks the connection and gives an error if it fails.
+if ($conn->connect_error)
+{
+  die("Connection failed: " . $conn->connect_error);
+} 
+
+// Gets all the data from the users table of the database.
+// The query for the database.
+$query = "SELECT VRScene, VoiceType FROM users WHERE UserName='$name';";
+
+// Querys the database.
+mysqli_query($conn, $query) or die("Q Error");
+
+// The result of the database query.
+$result = mysqli_query($conn, $query);
+
+// An array of the result of the database query.
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+$vr_scene = $row['VRScene'];
+$voice_type = $row['VoiceType'];
+
+?>
 
 <script type="text/javascript">
   function startAudio()
   {
-    if (name == 'bb')
+    var name = "<?php echo $name; ?>";
+    var vr_scene = "<?php echo $vr_scene; ?>";
+    var voice_type = "<?php echo $voice_type; ?>";
+
+    if (vr_scene == 'bb')
     {
-      var text = "This is bb." // beautifulBeachText;
+      var text = beautifulBeachText // beautifulBeachText;
     }
-    else if (name == 'ff')
+    else if (vr_scene == 'ff')
     {
-      var text = "This is ff." // fantasticalForestText;
+      var text = fantasticalForestText // fantasticalForestText;
     }
 
-    responsiveVoice.speak("", "UK English Male");
+    responsiveVoice.speak(text, voice_type);
   }
 </script>
 
